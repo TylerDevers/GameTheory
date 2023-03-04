bcd50a7ab870ec9e56325a75877ca703e1d964df
Tyler Devers <TylerDevers@users.noreply.github.com>
2023-03-03 20:52:09 -0800

    add title screen, meet branching criteria

---
+++ GameTheory/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    
+
+
+
+    public void StartGame() {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+
+}
